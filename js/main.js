0735e81266f4e6e1097aef61d8aaeabfc4ff1cf7
sergiojerezrivera <dawsergiojerez@gmail.com>
2019-01-20 20:06:27 +0000

    Added functionality of posts printed on screen from a json

--- js/main.js
+++ js/main.js
@@ -13,6 +13,61 @@ $(document).ready(function() {
 
 
 
+      var posts = [
+            {
+                title:    "Title number 1",
+                date:    'Published on the '+moment().format("Do") + " of " + moment().format("MMMM YYYY"),
+                content:  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam soluta quas facilis ex voluptates fuga corrupti, eum vero dolorem impedit delectus doloremque accusamus ullam quam molestiae deleniti fugiat velit iusto expedita totam harum modi quos a! Dignissimos sed nostrum ratione totam, ut soluta quod numquam non hic animi vero et!" 
+            },
+            {
+                title:    "Title number 2",
+                date:    'Published on the '+moment().format("Do") + " of " + moment().format("MMMM YYYY"),
+                content:  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam soluta quas facilis ex voluptates fuga corrupti, eum vero dolorem impedit delectus doloremque accusamus ullam quam molestiae deleniti fugiat velit iusto expedita totam harum modi quos a! Dignissimos sed nostrum ratione totam, ut soluta quod numquam non hic animi vero et!" 
+            },
+            {
+                title:    "Title number 3",
+                date:    'Published on the '+moment().format("Do") + " of " + moment().format("MMMM YYYY"),
+                content:  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam soluta quas facilis ex voluptates fuga corrupti, eum vero dolorem impedit delectus doloremque accusamus ullam quam molestiae deleniti fugiat velit iusto expedita totam harum modi quos a! Dignissimos sed nostrum ratione totam, ut soluta quod numquam non hic animi vero et!" 
+            },
+            {
+                title:    "Title number 4",
+                date:    'Published on the '+moment().format("Do") + " of " + moment().format("MMMM YYYY"),
+                content:  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam soluta quas facilis ex voluptates fuga corrupti, eum vero dolorem impedit delectus doloremque accusamus ullam quam molestiae deleniti fugiat velit iusto expedita totam harum modi quos a! Dignissimos sed nostrum ratione totam, ut soluta quod numquam non hic animi vero et!" 
+            },
+            {
+                title:    "Title number 5",
+                date:    'Published on the '+moment().format("Do") + " of " + moment().format("MMMM YYYY"),
+                content:  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam soluta quas facilis ex voluptates fuga corrupti, eum vero dolorem impedit delectus doloremque accusamus ullam quam molestiae deleniti fugiat velit iusto expedita totam harum modi quos a! Dignissimos sed nostrum ratione totam, ut soluta quod numquam non hic animi vero et!" 
+            }
+
+        ];
+
+        
+        posts.forEach((item, index) => {
+            var post = `
+            
+            <article class="post">
+
+                <h2>${item.title}</h2>
+                <span class="date">${item.date}</span>
+
+                <p>
+                    ${item.content}
+                </p>
+
+                <a href="#" class="button-more">Leer Mas</a>
+
+            </article>
+            
+            `;
+            $("#posts").append(post);
+        });
+
+
+
+
+
+
 
 
   });

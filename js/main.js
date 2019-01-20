0c2b46c667d64ce24f6a9932caaeb15b9e3088bb
sergiojerezrivera <dawsergiojerez@gmail.com>
2019-01-20 19:08:53 +0000

    Slider Added

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+
+    $('.slider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 1000,
+        auto: true,
+        autoControls: false,
+        stopAutoOnClick: true,
+        pager: false
+      });
+
+
+
+
+
+  });
